4c2dad399e8aa49db601cd383428190e6c977a02
eli89h <eli89h@gmail.com>
2022-02-03 16:10:43 +0200

    added dropdown menu

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import produce from "immer";
 
 const cellOperationsConway = [
@@ -13,11 +13,23 @@ const cellOperationsConway = [
 ];
 
 const Grid = () => {
-  const [colAndRows, setColAndRows] = useState(10);
-  const numRows = colAndRows;
-  const numCols = colAndRows;
+  //const [colAndRows, setColAndRows] = useState(10);
+  const [numRows, setNumRows] = useState(10);
+  const [numCols, setNumCols] = useState(10);
 
-  const generateEmptyGrid = () => {
+  const [colAndRowsOptions] = useState([
+    { label: "6x6", value: 6 },
+    { label: "8x8", value: 8 },
+    { label: "10x10", value: 10 },
+    { label: "15x15", value: 15 },
+    { label: "20x20", value: 20 },
+    { label: "30x30", value: 30 },
+    { label: "50x50", value: 50 },
+    { label: "75x75", value: 75 },
+    { label: "100x100", value: 100 },
+  ]);
+
+  const generateGrid = () => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
       rows.push(Array.from(Array(numCols), () => 0));
@@ -26,9 +38,42 @@ const Grid = () => {
     return rows;
   };
 
+  const randomGrid = () => {
+    const myGrid = [];
+    for (let i = 0; i < numRows; i++) {
+      const row = [];
+      for (let j = 0; j < numCols; j++) {
+        row.push(Math.floor(Math.random() * 2));
+      }
+      myGrid.push(row);
+    }
+    return myGrid;
+  };
+
+  const handleChange = (newValue) => {
+    setNumRows(newValue);
+    setNumCols(newValue);
+    
+  };
+
   const [grid, setGrid] = useState(() => {
-    return generateEmptyGrid();
+    return randomGrid();
   });
+
+  useEffect(() => {
+    console.log(numRows);
+    console.log(numCols);
+    setGrid(randomGrid());
+    
+    // const generateNewGrid = () => {
+    //   const rows = [];
+    //   for (let i = 0; i < numRows; i++) {
+    //     rows.push(Array.from(Array(numCols), () => 0));
+    //   }
+    //   return rows;
+    // };
+    // setGrid(generateNewGrid());
+  }, [numRows]);
 
   const [running, setRunning] = useState(false);
 
@@ -69,17 +114,32 @@ const Grid = () => {
 
   return (
     <>
+      <select
+        onChange={(e) => handleChange(e.target.value)}
+        value={(numRows, numCols)}
+      >
+        {colAndRowsOptions.map((item) => (
+          <option
+            selected={item.label === "10x10"}
+            key={item.value}
+            value={item.value}
+            // onChange={(e) => handleChange(item.value)}
+          >
+            {item.label}
+          </option>
+        ))}
+      </select>
       <button
         onClick={() => {
           setRunning(!running);
           if (!running) {
-            const rows = [];
-            for (let i = 0; i < numRows; i++) {
-              rows.push(
-                Array.from(Array(numCols), () => (Math.random() < 0.3 ? 1 : 0))
-              );
-            }
-            setGrid(rows);
+            // const rows = [];
+            // for (let i = 0; i < numRows; i++) {
+            //   rows.push(
+            //     Array.from(Array(numCols), () => (Math.random() < 0.3 ? 1 : 0))
+            //   );
+            // }
+            setGrid(randomGrid());
             runningRef.current = true;
             runGame();
           }
@@ -94,12 +154,12 @@ const Grid = () => {
           // gridTemplateRows: `repeat(${numRows}, 40px)`,
         }}
       >
-        {grid.map((rows, i) =>
-          rows.map((col, j) => (
+        {grid && grid.map((rows, i) =>
+          rows && rows.map((col, j) => (
             <div
-              key={`${i}-${j}`}
+              // key={`${i}-${j}`}
               onClick={() => {
-                const newGrid = produce(grid, (gridCopy) => {
+                let newGrid = produce(grid, (gridCopy) => {
                   gridCopy[i][j] = grid[i][j] ? 0 : 1;
                 });
                 setGrid(newGrid);
